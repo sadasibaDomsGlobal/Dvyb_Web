bc7cfe7a15465ae3dbd3a1f3862c3bc942971070
DomsGlobal <domsgloballlp@gmail.com>
2025-11-04 11:16:17 +0530

    FAQ

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Footer from "./components/common/footer/footer";
+import { default as FaqPage } from "./components/common/FAQ/FAQ";
 
 export default function App() {
   return (
     <div className=" " >
   
-    <Footer />
+    {/* <Footer /> */}
+    <FaqPage />
 
     </div>
   );
