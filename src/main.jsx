c7f9b899c2bc5a9859ab1adef7dd20054d4eb32a
sudheer-ganta1 <sudheerganta.work@gmail.com>
2025-11-11 15:03:34 +0530

    added scrollToTop

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+
+import ScrollToTop from "./components/ScrollToTop";
 import App from "./App.jsx";
 
 // Import all CSS files
@@ -13,10 +15,10 @@ import "./styles/index.css";
 // import "./styles/components/cards.css";
 // import "./styles/components/sections.css";
 
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </StrictMode>
