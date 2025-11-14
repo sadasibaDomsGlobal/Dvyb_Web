837e605b1a161d334372416304cd2d24ca982ac4
Sadasiba Baliyarsingh <sadasiba.domsgloballlp@gmail.com>
2025-11-14 12:42:17 +0530

    UI design in progress

--- src/components/b2c/products/ProductGrid.jsx
+++ src/components/b2c/products/ProductGrid.jsx
@@ -32,7 +32,7 @@ const ProductGrid = ({ products }) => {
   return (
     <div >
       {/* Product Grid */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-[24px]">
         {currentProducts.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
