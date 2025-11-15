b206c8b6e62026695b9354eafff2dbfd735767f2
Sadasiba Baliyarsingh <sadasiba.domsgloballlp@gmail.com>
2025-11-15 12:50:37 +0530

    modified the 'B2C' to 'b2c'

--- src/pages/b2c/Profilepages/ProfilePage.jsx
+++ src/pages/b2c/Profilepages/ProfilePage.jsx
@@ -2,12 +2,13 @@
 import React, { useState, useEffect } from "react";
 import { useLocation, useSearchParams } from "react-router-dom";
 import { ArrowLeft } from "lucide-react";
-import Sidebar from '../../../components/B2C/ProfilePage/Sidebar';
-import MyInfo from "../../../components/B2C/ProfilePage/MyInfo";
+import Sidebar from '../../../components/b2c/ProfilePage/Sidebar';
+// import Sidebar from '../../../components/B2C/ProfilePage/Sidebar';
+import MyInfo from "../../../components/b2c/ProfilePage/MyInfo";
 import ProfileImage from "../../../components/B2C/ProfilePage/PhotoManager";
-import MyOrders from "../../../components/B2C/ProfilePage/Orders";
+import MyOrders from "../../../components/b2c/ProfilePage/Orders";
 import { WishlistPage } from "../WishlistPage/WishlistPage";
-import TryOnGallery from "../../../components/B2C/ProfilePage/TryOnGallery";
+import TryOnGallery from "../../../components/b2c/ProfilePage/TryOnGallery";
 
 const ProfilePage = () => {
   const [activeTab, setActiveTab] = useState("menu"); // Start with menu on mobile
